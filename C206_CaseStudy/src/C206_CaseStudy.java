da812a33b5fb3744c6509e996a9b7c13fd3b5687
21011560 <21011560@DESKTOP-Q6M1Q6O>
2022-08-02 16:39:58 +0800

    Elin first commit

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -17,11 +17,31 @@ public class C206_CaseStudy {
 			option = Helper.readInt("Enter an option > ");
 
 			if (option == 1) {
+				C206_CaseStudy.setHeader("VIEW");			
+				C206_CaseStudy.setHeader("VIEW ITEM");
+				System.out.println("1. View User");
+				System.out.println("2. View Category");
+				System.out.println("3. View Item");
+				System.out.println("4. View Bid");
+				System.out.println("5. View Deal");
+				
+				int viewOption = Helper.readInt("Enter option to view item type > ");
+				
+				if (viewOption == 1) {
 //				C206_CaseStudy.viewAllUser(); Jing En
-//				C206_CaseStudy.viewAllCategory(); Elin
+					
+				}else if (viewOption == 2) {
+				C206_CaseStudy.displayAllCategory(categoryList);
+				
+				}else if (viewOption == 3) {
 //				C206_CaseStudy.viewAllItem(); Shaun
+					
+				}else if (viewOption == 4) {
 				C206_CaseStudy.viewAllBid(bidList);
+				
+				}else if (viewOption == 5) {
 //				C206_CaseStudy.viewAllDeal(); Imran
+				}
 			}
 			else if (option == 2) {
 				C206_CaseStudy.setHeader("ADD");			
@@ -41,7 +61,9 @@ public class C206_CaseStudy {
 				}
 				else if (addOption == 2) {
 				// Add Category (Elin)
-					
+			    Category c = inputCategory();
+			    C206_CaseStudy.addCategory(categoryList, c);
+			    System.out.println("Category added");	
 				}
 				else if (addOption == 3) {
 				// Add Item (Shaun)
@@ -77,6 +99,7 @@ public class C206_CaseStudy {
 				}
 				else if (deleteOption == 2) {
 				// Delete Category (Elin)
+				C206_CaseStudy.deleteCategory(categoryList);
 					
 				}
 				else if (deleteOption == 3) {
@@ -97,6 +120,57 @@ public class C206_CaseStudy {
 			}
 		}
 	}
+	
+	
+	//================================= Add Category =================================
+	public static Category inputCategory() {
+		String category = Helper.readString("Enter Category Name > ");
+
+		Category c= new Category(category);
+		return c;
+		
+	}
+	public static void addCategory(ArrayList<Category> categoryList, Category c) {
+		
+		categoryList.add(c);
+		
+	}
+	
+	//================================= Display All Category =================================
+	public static String retrieveAllCategory(ArrayList<Category> categoryList) {
+		String output = "";
+
+		for (int i = 0; i < categoryList.size(); i++) {
+
+			output += String.format("%s \n", categoryList.get(i).getName());
+		}
+		return output;
+	}
+	public static void displayAllCategory(ArrayList<Category> categoryList) {
+		C206_CaseStudy.setHeader("CATEGORY LIST");
+		String output = String.format("%-10s \n", "CATEGORY NAME");
+		 output += retrieveAllCategory(categoryList);	
+		System.out.println(output);
+	}
+
+	//================================= Delete Category Based On Name =================================
+	public static void deleteCategory(ArrayList<Category> categoryList) {
+		String category = Helper.readString("Enter Category Name to delete > ");
+		
+		for (int i = 0; i < categoryList.size(); i++) {
+			
+			if (categoryList.get(i).getName().equals(category)){
+				categoryList.remove(i);
+				
+				
+			} else {
+				System.out.println("Category does not exist");
+				break;
+			}
+		}	
+	}
+	
+	
 	// ===================================== Delete Bid =====================================
 	private static void deleteBid(ArrayList<Bid> bidList) {
 		// TODO Auto-generated method stub
@@ -125,7 +199,7 @@ public class C206_CaseStudy {
 		}
 		return isDeleted;
 	}
-
+	// =========================================== View Bid ===========================================
 	private static void viewAllBid(ArrayList<Bid> bidList) {
 		// TODO Auto-generated method stub
 		C206_CaseStudy.setHeader("View All Bid List");
@@ -148,9 +222,9 @@ public class C206_CaseStudy {
 
 	public static void menu() {
 		C206_CaseStudy.setHeader("Campus Online Auction Shop (COAS)");
-		System.out.println("1. View All Item");
-		System.out.println("2. Add Item");
-		System.out.println("3. Delete item");
+		System.out.println("1. VIEW");
+		System.out.println("2. ADD");
+		System.out.println("3. DELETE");
 		System.out.println("4. Quit");
 		Helper.line(80, "-");
 
